3eebd495a8f60d0af900df29b6b53f621768854d
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-05-28 22:59:52 +0100

    Make the website title look consistent across all pages

--- header.php
+++ header.php
@@ -28,15 +28,9 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a class="link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a class="link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+			?>
+			<h1 class="site-title"><a class="link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php
 			$lblog_description = get_bloginfo( 'description', 'display' );
 			if ( $lblog_description || is_customize_preview() ) :
 				?>
