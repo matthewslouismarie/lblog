4380b0a6b75332d6d5f9287ebf3960ade9a4913c
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-06-02 18:05:33 +0200

    Add grunt sass to bootstrap

--- tests/AssetTest.php
+++ tests/AssetTest.php
@@ -3,13 +3,20 @@
 use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
-class GlyphiconTest extends TestCase
+class AssetTest extends TestCase
 {
-    public function test()
+    public function testGlyphicon()
     {
         $client = new Client();
         $res = $client->request('GET', 'http://localhost/wp-content/themes/lblog/public/glyphicons_free/glyphicons/png/glyphicons-517-menu-hamburger.png');
         $this->assertContains('image/png', $res->getHeader('content-type'));
+    }
+
+    public function testGruntSass()
+    {
+        $client = new Client();
+        $res = $client->request('GET', 'http://localhost/wp-content/themes/lblog/style.min.css');
+        $this->assertContains('text/css', $res->getHeader('content-type'));
     }
 }
 
