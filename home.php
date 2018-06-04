e31e018b450bad740a8aafff585e7ace1acd2241
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-06-04 15:27:52 +0200

    Force the excerpt to be shown in homepage

---
+++ home.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package lblog
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+?>
+<div>
+    <h2>
+      <a href="<?= get_the_permalink() ?>"><?= get_the_title(); ?></a>
+    </h2>
+    <?= get_the_excerpt(); ?>
+</div>
+<?php
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
