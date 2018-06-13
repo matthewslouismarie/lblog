0535787ada45c3563dceeacb566a4a2c96f67771
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-06-13 13:15:16 +0200

    Use the same permalink structure as the live server

---
+++ tests/PermalinkStructureTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+
+class PermalinkStructureTest extends TestCase
+{
+    public function test()
+    {
+        $client = new Client();
+        $res = $client->request('GET', 'http://localhost/page/1/');
+        $this->assertEquals(302, $res->getStatusCode());
+    }
+}
+
