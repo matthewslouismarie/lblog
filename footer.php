37ba1e08d9fbe962acabdf7ec8c5f9351d3bc019
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2018-06-13 12:19:01 +0200

    Fix Glyphicons wrong URL

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">Icons from <a href="http://glyphicons.com/">Glyphicon.com</a> (<a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>).</div>
+		<div class="site-info">Icons from <a href="http://glyphicons.com/">Glyphicons.com</a> (<a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>).</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
